af9a90125ac9e6bc5a5ac0d1b7ae3d4c235eb301
ksywxd <xenia5224@gmail.com>
2025-09-13 03:25:39 +0300

    part1

--- Task_4.cpp
+++ Task_4.cpp
@@ -1,8 +1,26 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
+    int X, Y;
+    cin >> X >> Y;
+    if (X == Y) {
+        X = 0; Y = 0;
+    }
+    else {
+        if (Y < X) {
+            Y = 0;
+        }
+        else {
+            X = 0;
+        }
+            
+        
+    }
+    
 
+        cout << X<< " "<<Y;
 
     return 0;
 }
