78935fe71e3d81cde63320c78555102850088f91
ksywxd <xenia5224@gmail.com>
2025-09-13 03:57:36 +0300

    part2

--- Task_4.cpp
+++ Task_4.cpp
@@ -9,18 +9,22 @@ int main()
         X = 0; Y = 0;
     }
     else {
-        if (Y < X) {
-            Y = 0;
-        }
-        else {
-            X = 0;
-        }
-            
-        
+        if (Y < X) Y = 0;
+        else X = 0;
     }
-    
 
-        cout << X<< " "<<Y;
+    double A, B, C, K;
+    cin >> A >> B >> C >> K;
+    if (A == B || A == C || B == C) cout << "A B C must be different"<<endl;
+    else {
+        double max = A;
+        if (B > max) max = B;
+        if (C > max) max = C;
+        if (A == max) A -= K;
+        if (B == max) B -= K;
+        if (C == max) C -= K;
+    }
+    cout << X << " " << Y << " " << A << " " << B << " " << C;
 
     return 0;
 }
